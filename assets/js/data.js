3557781b1639ec60abd4d59fe18c635f1d2ad234
Torobaev23 <daniyartorobaev23@gmail.com>
2024-04-24 15:33:37 +0600

    музыкальный плеер

---
+++ assets/js/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        id: 1,
+        link: "beat1.mp3",
+        genre: "Rap",
+        track: "Baby Mama",
+        group: "Скриптонит & Райда  ",
+        year: 2019,
+    },
+    {
+        id: 2,
+        link: "beat2.mp3",
+        genre: "Rap",
+        track: "Расстояние",
+        group: "ulukmanapo & bakr",
+        year: 2022,
+    },
+    {
+        id: 3,
+        link: "beat3.mp3",
+        genre: "Rap",
+        track: "Praise the Lord (Da Shine)",
+        group: "A$AP Rocky",
+        year: 2018,
+    },
+    {
+        id: 4,
+        link: "beat4.mp3",
+        genre: "Rap / Hip-Hop",
+        track: "Redrum",
+        group: "21 savage",
+        year: 2024,
+    },
+];
